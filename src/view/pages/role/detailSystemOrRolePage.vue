2a57edbdc6883a716fc4a1ab1c3bdf291155e8fd
1956025812@qq.com <1956025812@qq.com>
2019-04-13 16:54:55 +0800

    角色右上侧系统或角色详情OK

--- src/view/pages/role/detailSystemOrRolePage.vue
+++ src/view/pages/role/detailSystemOrRolePage.vue
@@ -1,7 +1,57 @@
 <template>
   <div>
     <div v-if="showDetail == 'system'">
-      <Card :bordered="false" title="系统详情"></Card>
+      <Card :bordered="false" title="系统详情">
+        <Form :label-width="80">
+          <Row>
+            <Col span="10" style="float: left">
+              <FormItem label="系统名称">
+                <Input readonly v-model="systemSystemName"/>
+              </FormItem>
+            </Col>
+            <Col span="10" style="float: right">
+              <FormItem label="系统标识">
+                <Input readonly v-model="systemSystemKey"/>
+              </FormItem>
+            </Col>
+          </Row>
+          <Row>
+            <Col span="10" style="float: left">
+              <FormItem label="创建人">
+                <Input readonly v-model="systemCreateName"/>
+              </FormItem>
+            </Col>
+            <Col span="10" style="float: right">
+              <FormItem label="创建时间">
+                <Input readonly v-model="systemCreateTime"/>
+              </FormItem>
+            </Col>
+          </Row>
+          <Row>
+            <Col span="10" style="float: left">
+              <FormItem label="修改人">
+                <Input readonly v-model="systemUpdateName"/>
+              </FormItem>
+            </Col>
+            <Col span="10" style="float: right">
+              <FormItem label="修改时间">
+                <Input readonly v-model="systemUpdateTime"/>
+              </FormItem>
+            </Col>
+          </Row>
+          <Row>
+            <FormItem label="描述">
+              <Input
+                type="textarea"
+                :autosize="true"
+                style="width: 100%"
+                readonly
+                v-model="systemDescription"
+              />
+            </FormItem>
+          </Row>
+        </Form>
+      </Card>
     </div>
     <div v-if="showDetail == 'role'">
       <Card :bordered="false" title="角色详情">
@@ -9,14 +59,14 @@
           <Row>
             <Col span="10" style="float: left">
               <FormItem label="角色名称">
-                <Input readonly v-model="roleName"/>
+                <Input readonly v-model="roleRoleName"/>
               </FormItem>
             </Col>
           </Row>
           <Row>
             <Col span="10" style="float: left">
               <FormItem label="所属系统">
-                <Input readonly v-model="systemName"/>
+                <Input readonly v-model="roleSystemName"/>
               </FormItem>
             </Col>
             <Col span="10" style="float: right">
@@ -28,24 +78,24 @@
           <Row>
             <Col span="10" style="float: left">
               <FormItem label="创建人">
-                <Input readonly v-model="createName"/>
+                <Input readonly v-model="roleCreateName"/>
               </FormItem>
             </Col>
             <Col span="10" style="float: right">
               <FormItem label="创建时间">
-                <Input readonly v-model="createTime"/>
+                <Input readonly v-model="roleCreateTime"/>
               </FormItem>
             </Col>
           </Row>
           <Row>
             <Col span="10" style="float: left">
               <FormItem label="修改人">
-                <Input readonly v-model="updateName"/>
+                <Input readonly v-model="roleUpdateName"/>
               </FormItem>
             </Col>
             <Col span="10" style="float: right">
               <FormItem label="修改时间">
-                <Input readonly v-model="updateTime"/>
+                <Input readonly v-model="roleUpdateTime"/>
               </FormItem>
             </Col>
           </Row>
@@ -56,7 +106,7 @@
                 :autosize="true"
                 style="width: 100%"
                 readonly
-                v-model="description"
+                v-model="roleDescription"
               />
             </FormItem>
           </Row>
@@ -70,6 +120,7 @@
 
 <script>
 import { getToken } from "@/libs/util";
+import { selectSystemDetailAPI } from "@/api/system/system";
 import { selectRoleDetailAPI } from "@/api/role/role";
 
 export default {
@@ -77,14 +128,21 @@ export default {
   data() {
     return {
       showDetail: "",
-      roleName: "",
-      systemName: "",
+      systemSystemKey: "",
+      systemSystemName: "",
+      systemCreateName: "",
+      systemCreateTime: "",
+      systemUpdateName: "",
+      systemUpdateTime: "",
+      systemDescription: "",
+      roleRoleName: "",
+      roleSystemName: "",
       parentRoleName: "",
-      createName: "",
-      createTime: "",
-      updateName: "",
-      updateTime: "",
-      description: ""
+      roleCreateName: "",
+      roleCreateTime: "",
+      roleUpdateName: "",
+      roleUpdateTime: "",
+      roleDescription: ""
     };
   },
 
@@ -95,26 +153,46 @@ export default {
     selectSystemOrRoleDetail(node) {
       this.showDetail = node.type;
 
-      /* var rid = this.$route.params.rid;
-      let params = new Object();
-      params.loginUid = getToken();
-      params.rid = rid;
-      selectRoleDetailAPI(params).then(res => {
-        if (res.data.code == 1) {
-          this.roleName = res.data.data.roleName;
-          this.systemName = res.data.data.systemName;
-          this.parentRoleName = res.data.data.parentRoleName;
-          this.createName = res.data.data.createName;
-          this.createTime = res.data.data.createTime;
-          this.updateName = res.data.data.updateName;
-          this.updateTime = res.data.data.updateTime;
-          this.description = res.data.data.description;
-        } else if (res.data.code == 0) {
-          this.$Notice.error({
-            desc: res.data.msg
-          });
-        }
-      }); */
+      if (node.type == "system") {
+        let params = new Object();
+        params.loginUid = getToken();
+        params.systemKey = node.systemKey;
+        selectSystemDetailAPI(params).then(res => {
+          if (res.data.code == 1) {
+            this.systemSystemName = res.data.data.systemName;
+            this.systemSystemKey = res.data.data.systemKey;
+            this.systemCreateName = res.data.data.createName;
+            this.systemCreateTime = res.data.data.createTime;
+            this.systemUpdateName = res.data.data.updateName;
+            this.systemUpdateTime = res.data.data.updateTime;
+            this.systemDescription = res.data.data.description;
+          } else if (res.data.code == 0) {
+            this.$Notice.error({
+              desc: res.data.msg
+            });
+          }
+        });
+      } else if (node.type == "role") {
+        let params = new Object();
+        params.loginUid = getToken();
+        params.rid = node.rid;
+        selectRoleDetailAPI(params).then(res => {
+          if (res.data.code == 1) {
+            this.roleRoleName = res.data.data.roleName;
+            this.roleSystemName = res.data.data.systemName;
+            this.parentRoleName = res.data.data.parentRoleName;
+            this.roleCreateName = res.data.data.createName;
+            this.roleCreateTime = res.data.data.createTime;
+            this.roleUpdateName = res.data.data.updateName;
+            this.roleUpdateTime = res.data.data.updateTime;
+            this.roleDescription = res.data.data.description;
+          } else if (res.data.code == 0) {
+            this.$Notice.error({
+              desc: res.data.msg
+            });
+          }
+        });
+      }
     }
   },
 
